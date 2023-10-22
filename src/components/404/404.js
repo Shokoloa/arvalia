a9f698f01d51f1e7537d7609009b3c083748888f
Shoko <97951985+Shokoloa@users.noreply.github.com>
2023-10-22 19:29:15 +0200

    Create 404.js

---
+++ src/components/404/404.js
@@ -0,0 +1,31 @@
+import { Helmet } from 'react-helmet';
+import React from 'react';
+import './404.css';
+
+const NotFound = () => {
+  return (
+    <section>
+      <Helmet>
+        <title>Arvalia | 404 Not Found</title>
+        <meta name="og:site_name" content="Oh ! Il semblerait que ceci est la page 404 d'Arvalia, cette page est inutile pour vous !"></meta>
+      </Helmet>
+      <div class="bg"></div>
+      <main>
+        <div class="container">
+          <div class="row">
+            <div>
+              
+            </div>
+            <div>
+              <h1>Oups ! Vous vous êtes perdu.</h1>
+              <p>La page que vous recherchez n'existe pas. Comment êtes-vous arrivé ici est un mystère.</p>
+              <button onclick="window.location.href = '/'">Accueil</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default NotFound;
