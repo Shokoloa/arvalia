decd0d383f9eaf1f18512cc28f8fac04d1117f60
Shoko <97951985+Shokoloa@users.noreply.github.com>
2023-10-22 19:32:41 +0200

    Add files via upload

---
+++ src/components/News/Articles.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import webUpdate from '../../Assets/news/web_update.png';
+
+function Articles({ data }) {
+    const [expandedArticles, setExpandedArticles] = useState([]);
+
+    const toggleArticleExpansion = (index) => {
+        if (expandedArticles.includes(index)) {
+            setExpandedArticles(expandedArticles.filter((item) => item !== index));
+        } else {
+            setExpandedArticles([...expandedArticles, index]);
+        }
+    };
+
+    return (
+        <div id="news-panel">
+            {data.map((article, index) => {
+                const isExpanded = expandedArticles.includes(index);
+                let lowDesc = article.desc.substring(0, 30) + "...";
+                let img;
+
+                if (article.class === "webUpdate") {
+                    img = webUpdate;
+                };
+
+                return (
+                    <article className={`news-card ${isExpanded ? 'news-card-active' : ''}`} key={index}>
+                        <img src={img} draggable="false" alt="" />
+                        <div className="date">
+                            <p>{article.date}</p>
+                        </div>
+                        <figcaption>
+                            <h1>{article.name}</h1>
+                            <p className="lowDesc" style={{ display: isExpanded ? 'none' : 'flex' }}>{lowDesc}</p>
+                            <p className="globalDesc" style={{ display: isExpanded ? 'flex' : 'none' }}>{article.desc}</p>
+                            <button className="read-more" onClick={() => toggleArticleExpansion(index)}>
+                                {isExpanded ? 'Voir moins' : 'Voir plus'}
+                            </button>
+                        </figcaption>
+                    </article>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Articles;
