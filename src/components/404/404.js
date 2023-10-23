acbad4fa60d8e8dc9bf548013ea394fdcf3a6719
Shoko <97951985+Shokoloa@users.noreply.github.com>
2023-10-23 21:08:55 +0200

    Add files via upload

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -1,24 +1,28 @@
 import { Helmet } from 'react-helmet';
 import React from 'react';
+import { useLocation } from "react-router-dom";
+import './404.css';
+import SVG from './SVGAnimation';
 
 const NotFound = () => {
+  let location = useLocation();
+
   return (
     <section>
       <Helmet>
         <title>Arvalia | 404 Not Found</title>
         <meta name="og:site_name" content="Oh ! Il semblerait que ceci est la page 404 d'Arvalia, cette page est inutile pour vous !"></meta>
       </Helmet>
-      <div class="bg"></div>
       <main>
         <div class="container">
           <div class="row">
             <div>
-              
+              <SVG />
             </div>
             <div>
               <h1>Oups ! Vous vous êtes perdu.</h1>
-              <p>La page que vous recherchez n'existe pas. Comment êtes-vous arrivé ici est un mystère.</p>
-              <button onclick="window.location.href = '/'">Accueil</button>
+              <p>La page <code>{location.pathname}</code> n'existe pas. Comment êtes-vous arrivé ici est un mystère.</p>
+              <button onClick={() => window.location.href = '/'}>Retourner à l'accueil</button>
             </div>
           </div>
         </div>
