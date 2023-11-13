119ca760d47b347df4a5e7dd6a39b767e9ee396c
lebakasable <hugogole.ito@gmail.com>
2023-11-13 16:39:10 +0100

    fix: remove js code for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,35 @@
+import { useState } from "react";
 import logo from "../assets/logo/arvalia-nobg.png";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => (
-  <header id="nav-menu" aria-label="navigation bar">
-    <Link to="/" className="logo">
-      <img src={logo} draggable={false} alt="Logo" />
-    </Link>
-    <section className="navcontainer">
-      <article className="nav-start">
-        <nav className="menu">
-          <ul className="menu-bar">
-            <li><Link to="/" className="nav-link">Accueil</Link></li>
-            <li><Link to="/news" className="nav-link">Actualités</Link></li>
-            <li><Link to="/histoire" className="nav-link">Histoire</Link></li>
-            <li className="nav-lastlink"><Link to="/phoenix-rise" className="nav-link">Phoenix Rise</Link></li>
-            <li><Link to="/phoenix-rise" className="nav-link phone">Phoenix Rise</Link></li>
-          </ul>
-        </nav>
-      </article>
-      <label className="nav-end">
-        <input type="checkbox" id="hamburger" />
-        <span></span>
-        <span></span>
-        <span></span>
-      </label>
-    </section>
-  </header>
-);
+export const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <header id="nav-menu" aria-label="navigation bar">
+      <Link to="/" className="logo">
+        <img src={logo} draggable={false} alt="Logo" />
+      </Link>
+      <section className="navcontainer">
+        <article className="nav-start">
+          <nav className={`menu ${show ? 'show' : ''}`}>
+            <ul className="menu-bar">
+              <li><Link to="/" className="nav-link">Accueil</Link></li>
+              <li><Link to="/news" className="nav-link">Actualités</Link></li>
+              <li><Link to="/histoire" className="nav-link">Histoire</Link></li>
+              <li className="nav-lastlink"><Link to="/phoenix-rise" className="nav-link">Phoenix Rise</Link></li>
+              <li><Link to="/phoenix-rise" className="nav-link phone">Phoenix Rise</Link></li>
+            </ul>
+          </nav>
+        </article>
+        <label className="nav-end">
+          <input type="checkbox" id="hamburger" onClick={() => setShow(!show)} />
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </section>
+    </header>
+  );
+};
 
